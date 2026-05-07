78252d63e18af2608d6df619cd88425786cf7b70
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-05-07 12:11:17 +0600

    relationship

--- prisma/migrations/20260507052257_remove_unique_from_authorid/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Post_autherId_key";
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_autherId_fkey" FOREIGN KEY ("autherId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

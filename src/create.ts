b57da82faf051f14c89ed6d41076b73783e61b82
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-05-07 10:47:27 +0600

    create,create many, find all this query learn

---
+++ src/create.ts
@@ -0,0 +1,29 @@
+import { prisma } from "./lib/prisma";
+
+const main = async () => {
+  // Create a new user with a post
+  const userMany = await prisma.user.createMany({
+    data: [
+      {
+        name: "kazi",
+        email: "kazi@example.com",
+      },
+      {
+        name: "kazi2",
+        email: "kazi2@example.com",
+      },
+    ],
+  });
+
+  console.log(userMany, "MANY");
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ src/find.ts
@@ -0,0 +1,31 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+  // Create a new user with a post
+  // const getAllUserFromDB = await prisma.user.findMany();
+  // console.log(getAllUserFromDB, "GET ALL");
+  // const findFirst = await prisma.user.findFirst({
+  //   where: {
+  //     name: "Alice",
+  //   },
+  // });
+  // console.log("Found user:", findFirst);
+
+  const findUnique = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: 1,
+    },
+  });
+
+  console.log({ findUnique });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
